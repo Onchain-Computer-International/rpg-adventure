e2936c3bd88a7b190c36f88fcbfcd150713985a8
raz <raz@zgen.hu>
2024-12-26 16:33:57 +0100

    hm

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
   resolve: {
     alias: {
-      'three': 'three'
+      'three': path.resolve(__dirname, 'node_modules/three/build/three.module.js')
     }
-  }
+  },
+  base: '/'
 }); 
