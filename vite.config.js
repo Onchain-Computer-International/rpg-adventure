b148dddc691cb1df03e81faacb38d3362fec0992
raz <raz@zgen.hu>
2024-12-26 16:37:03 +0100

    revert

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,9 @@
 import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
   resolve: {
     alias: {
-      'three': path.resolve(__dirname, 'node_modules/three/build/three.module.js')
+      'three': 'three'
     }
-  },
-  base: '/',
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          'three': ['three']
-        }
-      }
-    }
-  },
-  logLevel: 'info'
+  }
 }); 
